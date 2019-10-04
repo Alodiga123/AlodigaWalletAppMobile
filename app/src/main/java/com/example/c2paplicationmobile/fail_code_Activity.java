634efdad10f817dbbf6a93120954b91de2a05a4b
adiquintero <yquintero@alodiga.com>
2019-10-04 16:26:02 -0400

    ADD busqueda del objeto

--- app/src/main/java/com/example/c2paplicationmobile/fail_code_Activity.java
+++ app/src/main/java/com/example/c2paplicationmobile/fail_code_Activity.java
@@ -15,7 +15,7 @@ public class fail_code_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fail_code_operation_layout);
-
+        step1_next_button= findViewById(R.id.step1_next_button);
         step1_next_button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                     Intent i = new Intent(fail_code_Activity.this, MainActivity.class);
