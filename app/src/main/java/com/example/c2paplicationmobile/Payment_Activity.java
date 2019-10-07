947b6d3d22ec706fd7444d2512f20af46b87a802
adiquintero <yquintero@alodiga.com>
2019-10-07 14:29:00 -0400

    ADD salario al objeto spinner

--- app/src/main/java/com/example/c2paplicationmobile/Payment_Activity.java
+++ app/src/main/java/com/example/c2paplicationmobile/Payment_Activity.java
@@ -58,7 +58,7 @@ public class Payment_Activity extends AppCompatActivity {
         list_product = Session.getObjUserHasProducts();
         final ObjTransferMoney[] objTransferMoney = new ObjTransferMoney[list_product.size()];
         for(int i=0;i<list_product.size();i++){
-            objTransferMoney[i] = new ObjTransferMoney(list_product.get(i).getId(),list_product.get(i).getName().trim()+" "+ list_product.get(i).getSymbol().trim()+ " - "+list_product.get(i).getCurrentBalance() );
+            objTransferMoney[i] = new ObjTransferMoney(list_product.get(i).getId(),list_product.get(i).getName().trim()+" "+ list_product.get(i).getSymbol().trim()+ " - "+list_product.get(i).getCurrentBalance(),list_product.get(i).getCurrentBalance() );
         }
         //Llena tipos de cuenta
         //List<ObjTransferMoney> countries = new ArrayList<ObjTransferMoney>();
@@ -73,7 +73,7 @@ public class Payment_Activity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 ObjTransferMoney objTransferMoney1;
                 objTransferMoney1 = (ObjTransferMoney) spinnerIdentification.getSelectedItem();
-                currencySelected =new ObjUserHasProduct(objTransferMoney1.getId(),objTransferMoney1.getName());
+                currencySelected =new ObjUserHasProduct(objTransferMoney1.getId(),objTransferMoney1.getName(),objTransferMoney1.getCurrency());
                 Session.setMoneySelected(currencySelected);
                 /*if(objTransferMoney1.getId().equals("0")){
                     currencySelected = 0;
