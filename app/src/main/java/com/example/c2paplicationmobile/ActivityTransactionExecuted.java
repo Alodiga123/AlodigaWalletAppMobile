cc5b3c1123d8ca5b50f00affcf7c2ee8247c869c
Unknown <lgutierrez@alodiga.com>
2019-10-11 16:27:32 -0400

    actualizacion

--- app/src/main/java/com/example/c2paplicationmobile/ActivityTransactionExecuted.java
+++ app/src/main/java/com/example/c2paplicationmobile/ActivityTransactionExecuted.java
@@ -172,6 +172,7 @@ public  class ActivityTransactionExecuted extends AppCompatActivity implements O
                     responsetxt = "";
                     serviceStatus = false;
                 }else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_USER_NOT_HAS_TRANSACTIONS)){
+                    Toast.makeText(context, "La cuenta no posee movimientos asociados", Toast.LENGTH_LONG).show();
                     responsetxt = datosRespuesta;
                     serviceStatus = false;
                 }else if (responseCode.isEmpty()){
@@ -210,7 +211,15 @@ public  class ActivityTransactionExecuted extends AppCompatActivity implements O
 
         @Override
         protected void onPostExecute(Boolean result)
+
+
         {
+
+           /* if (result) {
+
+            } else {
+
+            }*/
             ArrayList<ObjTransaction> transactions = new ArrayList<ObjTransaction>();
 
             transactions = getParseResponse(stringResponse);
@@ -253,15 +262,6 @@ public  class ActivityTransactionExecuted extends AppCompatActivity implements O
                                 +"\n"
                                 +"Comision: "+((ObjTransaction)newsData.getObject()).getCommision()
                                 +"\n");
-/****************/
-
-                        Intent sendIntent = new Intent();
-                        sendIntent.setAction(Intent.ACTION_SEND);
-                        sendIntent.putExtra(Intent.EXTRA_TEXT, "Enviar a:");
-                        sendIntent.setType("text/plain");
-                        //startActivity(sendIntent);
-
-/**************/
                         alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int which) {
@@ -316,17 +316,5 @@ public  class ActivityTransactionExecuted extends AppCompatActivity implements O
             return results;
         }
     }
-
-    protected void backReturn() {
-
-        Intent i = new Intent(ActivityTransactionExecuted.this, MainActivity.class);
-        startActivity(i);
-        finish();
-    }
-
-
-
-
-
 }
 
