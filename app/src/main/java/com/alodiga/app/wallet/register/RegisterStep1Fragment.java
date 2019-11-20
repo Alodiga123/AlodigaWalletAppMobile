139e9e5bfd6585dde0412ff365e7c56267fb063f
adiquintero <yquintero@alodiga.com>
2019-11-20 14:25:04 -0400

    DEL cable de paises

--- app/src/main/java/com/alodiga/app/wallet/register/RegisterStep1Fragment.java
+++ app/src/main/java/com/alodiga/app/wallet/register/RegisterStep1Fragment.java
@@ -18,6 +18,7 @@ import com.alodiga.app.R;
 import com.alodiga.app.wallet.adapters.SpinAdapterCountry;
 import com.alodiga.app.wallet.login.LoginActivity;
 import com.alodiga.app.wallet.model.ObjCountry;
+import com.alodiga.app.wallet.model.ObjGenericObject;
 import com.alodiga.app.wallet.utils.Constants;
 import com.alodiga.app.wallet.utils.CustomToast;
 import com.alodiga.app.wallet.utils.ProgressDialogAlodiga;
@@ -45,6 +46,9 @@ public class RegisterStep1Fragment extends Fragment implements
     private String getMobileNumber = "";
     private ObjCountry objCountry;
     private RegisterStep1Fragment.UserSendSmsTask mAuthTask = null;
+    private UserSendcountryTask mAuthTaskCountry;
+    SoapObject response2;
+    SoapObject response;
 
     public RegisterStep1Fragment() {
 
@@ -57,51 +61,7 @@ public class RegisterStep1Fragment extends Fragment implements
                 false);
         initViews();
 
-        List<ObjCountry> countries = new ArrayList<ObjCountry>();
-        ObjCountry[] objCountry = new ObjCountry[40];
-        objCountry[0] = new ObjCountry("0", "País");
-        objCountry[1] = new ObjCountry("1", "Estados Unidos");
-        objCountry[2] = new ObjCountry("58", "Venezuela");
-        objCountry[3] = new ObjCountry("93", "Afganistán ");
-        objCountry[4] = new ObjCountry("355", "Albania ");
-        objCountry[5] = new ObjCountry("49", "Alemania ");
-        objCountry[6] = new ObjCountry("376", "Andorra ");
-        objCountry[7] = new ObjCountry("244", "Angola ");
-        objCountry[8] = new ObjCountry("1", "Antigua y Barbuda ");
-        objCountry[9] = new ObjCountry("966", "Arabia Saudita");
-        objCountry[10] = new ObjCountry("54", "Argentina");
-        objCountry[11] = new ObjCountry("374", "Armenia ");
-        objCountry[12] = new ObjCountry("43", "Australia ");
-        objCountry[13] = new ObjCountry("994", "Azerbaiyán ");
-        objCountry[14] = new ObjCountry("1", "Bahamas ");
-        objCountry[15] = new ObjCountry("880", "Bangladés ");
-        objCountry[16] = new ObjCountry("1", "Barbados ");
-        objCountry[17] = new ObjCountry("973", "Baréin ");
-        objCountry[18] = new ObjCountry("32", "Bélgica ");
-        objCountry[19] = new ObjCountry("501", "Belice ");
-        objCountry[20] = new ObjCountry("229", "Benin ");
-        objCountry[21] = new ObjCountry("375", "Bielorrusia ");
-        objCountry[22] = new ObjCountry("591", "Bolivia ");
-        objCountry[23] = new ObjCountry("387", "Bosnia y Herzegovina");
-        objCountry[24] = new ObjCountry("267", "Botsuana");
-        objCountry[25] = new ObjCountry("55", "Brasil");
-        objCountry[26] = new ObjCountry("673", "Brunéi");
-        objCountry[27] = new ObjCountry("359", "Bulgaria");
-        objCountry[28] = new ObjCountry("226", "Burkina Faso");
-        objCountry[29] = new ObjCountry("257", "Burundi");
-        objCountry[30] = new ObjCountry("975", "Bután");
-        objCountry[31] = new ObjCountry("238", "Cabo Verde");
-        objCountry[32] = new ObjCountry("855", "Camboya");
-        objCountry[33] = new ObjCountry("237", "Camerún");
-        objCountry[34] = new ObjCountry("1", "Canadá");
-        objCountry[35] = new ObjCountry("974", "Catar");
-        objCountry[36] = new ObjCountry("235", "Chad");
-        objCountry[37] = new ObjCountry("56", "Chile");
-        objCountry[38] = new ObjCountry("86", "China");
-        objCountry[39] = new ObjCountry("357", "Chipre");
-        SpinAdapterCountry spinAdapterCountry;
-        spinAdapterCountry = new SpinAdapterCountry(this.getContext(), android.R.layout.simple_spinner_item, objCountry);
-        spinnerCountry.setAdapter(spinAdapterCountry);
+        getCountries();
 
         setListeners();
         return view;
@@ -186,6 +146,14 @@ public class RegisterStep1Fragment extends Fragment implements
 
     }
 
+    public void getCountries() {
+        progressDialogAlodiga = new ProgressDialogAlodiga(getContext(), getString(R.string.loading));
+        progressDialogAlodiga.show();
+        mAuthTaskCountry = new UserSendcountryTask();
+        mAuthTaskCountry.execute((Void) null);
+
+    }
+
     /**
      * @param phone
      * @return
@@ -212,7 +180,7 @@ public class RegisterStep1Fragment extends Fragment implements
 
             WebService webService = new WebService();
             Utils utils = new Utils();
-            SoapObject response;
+
             try {
                 String responseCode;
                 String responseMessage = "";
@@ -327,4 +295,129 @@ public class RegisterStep1Fragment extends Fragment implements
         }
     }
 
+
+    public class UserSendcountryTask extends AsyncTask<Void, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+
+            WebService webService = new WebService();
+            Utils utils = new Utils();
+            try {
+                String responseCode;
+                String responseMessage = "";
+
+
+                HashMap<String, String> map = new HashMap<String, String>();
+
+                response2 = WebService.invokeGetAutoConfigString(map, Constants.WEB_SERVICES_METHOD_NAME_GET_COUNTRIES_, Constants.ALODIGA);
+                responseCode = response2.getProperty("codigoRespuesta").toString();
+                responseMessage = response2.getProperty("mensajeRespuesta").toString();
+
+
+                if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_EXITO)) {
+
+                    //movilCode = response2.getProperty("datosRespuesta").toString();
+                    responsetxt = getString(R.string.web_services_response_00);
+                    serviceStatus = true;
+                    return serviceStatus;
+
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_DATOS_INVALIDOS)) {
+                    responsetxt = getString(R.string.web_services_response_01);
+                    serviceStatus = false;
+
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_CONTRASENIA_EXPIRADA)) {
+                    responsetxt = getString(R.string.web_services_response_03);
+                    serviceStatus = false;
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_IP_NO_CONFIANZA)) {
+                    responsetxt = getString(R.string.web_services_response_04);
+                    serviceStatus = false;
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_CREDENCIALES_INVALIDAS)) {
+                    responsetxt = getString(R.string.web_services_response_05);
+                    serviceStatus = false;
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_USUARIO_BLOQUEADO)) {
+                    responsetxt = getString(R.string.web_services_response_06);
+                    serviceStatus = false;
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_NUMERO_TELEFONO_YA_EXISTE)) {
+                    responsetxt = getString(R.string.web_services_response_08);
+                    serviceStatus = false;
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_PRIMER_INGRESO)) {
+                    responsetxt = getString(R.string.web_services_response_12);
+                    serviceStatus = false;
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_USUARIO_SOSPECHOSO)) {
+                    responsetxt = getString(R.string.web_services_response_95);
+                    serviceStatus = false;
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_USUARIO_PENDIENTE)) {
+                    responsetxt = getString(R.string.web_services_response_96);
+                    serviceStatus = false;
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_USUARIO_NO_EXISTE)) {
+                    responsetxt = getString(R.string.web_services_response_97);
+                    serviceStatus = false;
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_ERROR_CREDENCIALES)) {
+                    responsetxt = getString(R.string.web_services_response_98);
+                    serviceStatus = false;
+                } else if (responseCode.equals(Constants.WEB_SERVICES_RESPONSE_CODE_ERROR_INTERNO)) {
+                    responsetxt = getString(R.string.web_services_response_99);
+                    serviceStatus = false;
+                } else {
+                    responsetxt = getString(R.string.web_services_response_99);
+                    serviceStatus = false;
+                }
+                //progressDialogAlodiga.dismiss();
+            } catch (IllegalArgumentException e) {
+                responsetxt = getString(R.string.web_services_response_99);
+                serviceStatus = false;
+                e.printStackTrace();
+                System.err.println(e);
+                return false;
+            } catch (Exception e) {
+                responsetxt = getString(R.string.web_services_response_99);
+                serviceStatus = false;
+                e.printStackTrace();
+                System.err.println(e);
+                return false;
+            }
+            return serviceStatus;
+
+        }
+
+        @Override
+        protected void onPostExecute(final Boolean success) {
+            mAuthTask = null;
+            //showProgress(false);
+            if (success) {
+
+                ObjCountry[] objCountry= getListCountry(response2);
+
+                SpinAdapterCountry spinAdapterCountry;
+                spinAdapterCountry = new SpinAdapterCountry(getContext(), android.R.layout.simple_spinner_item, objCountry);
+                spinnerCountry.setAdapter(spinAdapterCountry);
+
+            } else {
+                new CustomToast().Show_Toast(getActivity(), view,
+                        responsetxt);
+            }
+            progressDialogAlodiga.dismiss();
+        }
+
+        @Override
+        protected void onCancelled() {
+            mAuthTask = null;
+        }
+    }
+
+
+    protected ObjCountry[] getListCountry(SoapObject response) {
+
+        ObjCountry[] obj2 = new ObjCountry[response.getPropertyCount() - 3];
+
+        for (int i = 3; i < response.getPropertyCount(); i++) {
+            SoapObject obj = (SoapObject) response.getProperty(i);
+            String propiedad = response.getProperty(i).toString();
+            ObjCountry object = new ObjCountry(obj.getProperty("code").toString(),obj.getProperty("name").toString());
+            obj2[i - 3] = object;
+        }
+
+        return obj2;
+    }
 }
