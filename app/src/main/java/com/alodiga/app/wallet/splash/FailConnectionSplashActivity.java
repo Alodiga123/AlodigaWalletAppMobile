93c3eab0d642a72d398922d4adcb40536983b8c9
aquintero <yquintero@alodiga.com>
2020-08-26 15:14:23 -0400

    ajustes de interfaz de error de conexion

--- app/src/main/java/com/alodiga/app/wallet/splash/FailConnectionSplashActivity.java
+++ app/src/main/java/com/alodiga/app/wallet/splash/FailConnectionSplashActivity.java
@@ -10,14 +10,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.alodiga.app.R;
 
 public class FailConnectionSplashActivity extends AppCompatActivity {
-    private Button button;
+    private Button button, cancel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fail_connection);
+        cancel= findViewById(R.id.cancel);
         button = findViewById(R.id.btn_restart);
         button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(FailConnectionSplashActivity.this, SplashActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+        cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(FailConnectionSplashActivity.this, SplashActivity.class);
