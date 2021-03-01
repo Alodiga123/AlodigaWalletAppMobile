e522e1a1ba9b63b12fca2747e79db83d4081b150
aquintero <yquintero@alodiga.com>
2021-03-01 16:36:14 -0400

    code 8 de validacion

--- app/src/main/java/com/alodiga/app/wallet/validate/ValidateAccountStep8Activity.java
+++ app/src/main/java/com/alodiga/app/wallet/validate/ValidateAccountStep8Activity.java
@@ -14,18 +14,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ValidateAccountStep8Activity extends AppCompatActivity {
 
-    private Button finalize;
+    private Button next,back;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.kyc_code8_layout);
-        finalize= findViewById(R.id.finalize);
-
-        finalize.setOnClickListener(new View.OnClickListener() {
+        next= findViewById(R.id.next);
+        back=findViewById(R.id.backToLoginBtn);
+        back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(ValidateAccountStep8Activity.this, MainActivity.class);
+                startActivity(i);
+                finish();            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(ValidateAccountStep8Activity.this, ValidateAccountActivity.class);
                 startActivity(i);
                 finish();            }
         });
